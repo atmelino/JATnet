86654dbbd744144a1a25045f15a19d0885fca875
jatnik <btmithras@dt112345>
2014-10-04 20:57:36 -0500

    tomcat sample war from Apache web site

--- tomcat/install.sh
+++ tomcat/install.sh
@@ -10,8 +10,13 @@ echo "copy file"
 sudo cp HelloWorld.class /var/lib/tomcat7/webapps/ROOT/WEB-INF/classes
 
 echo "ls"
-
 ls /var/lib/tomcat7/webapps/ROOT/WEB-INF/classes
+
+
+
+cat /var/lib/tomcat7/webapps/ROOT/WEB-INF/web.xml
+
+
 
 echo press enter
 
