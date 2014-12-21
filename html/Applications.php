7d35f79f30d3becd194738c1e6f51b207c297dd8
jatnik <jatnik@jat>
2014-12-20 19:02:52 -0600

    promote Three.js solution and GUI

--- html/Applications.php
+++ html/Applications.php
@@ -6,17 +6,19 @@
 </head>
 <body>
 	<h2>Web Browser Applications</h2>
-	<table border="1" cellpadding="1" cellspacing="1"
-		style="width: 500px; border-collapse: collapse;">
+	<table border="1" cellpadding="1" cellspacing="1" style="width: 500px; border-collapse: collapse;">
 		<tbody>
 			<tr>
 				<td>Orbit Viewer<br> <a
-					href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8080/JATServlet/JS3DPlot/JS3DPlot_orbit02.html"
-					style="color: rgb(0, 98, 160); text-decoration: underline;"
-					target="_parent"><img alt="" src="images/orbitViewer.png"
-						style="width: 300px;" /> </a>
+					href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8080/JATServlet/orbitViewer/orbitViewer.html"
+					style="color: rgb(0, 98, 160); text-decoration: underline;" target="_parent"><img alt=""
+						src="images/orbitViewer.png" style="width: 300px;" /> </a>
 				</td>
-				<td></td>
+				<td>Orbit Viewer<br> <a
+					href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8080/JATServlet/JS3DPlot/JS3DPlot_orbit02.html"
+					style="color: rgb(0, 98, 160); text-decoration: underline;" target="_parent"><img alt=""
+						src="images/orbitViewerJS3DPlot.png" style="width: 300px;" /> </a>
+				</td>
 			</tr>
 			<tr>
 				<td></td>
@@ -25,8 +27,7 @@
 		</tbody>
 	</table>
 	<p>
-		<a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8080/JATServlet"
-			target="_parent">Web Apps</a>
+		<a href="http://<?php echo $_SERVER['SERVER_ADDR'];?>:8080/JATServlet" target="_parent">Web Apps</a>
 	</p>
 	<p>
 		<a href="develop/WebAppsDevelop.html" target="iframe_a">Web Apps Develop</a>
@@ -34,10 +35,9 @@
 
 	<h2>Java Applications</h2>
 	<p>
-		<iframe frameborder="1" height="1000" scrolling="yes"
-			src="jat/deploy/index.html" width="100%"></iframe>
+		<iframe frameborder="1" height="1000" scrolling="yes" src="jat/deploy/index.html" width="100%"></iframe>
 	</p>
 
-	
+
 </body>
 </html>
