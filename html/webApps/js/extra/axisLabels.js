2d28234ac00c274727f82d31c8460f4b23b39fe8
jatnik <jatnik@jat>
2014-12-22 20:56:39 -0600

    z axis up

--- html/webApps/js/extra/axisLabels.js
+++ html/webApps/js/extra/axisLabels.js
@@ -1,5 +1,14 @@
-function makeAxisLabel(scene) {
+function makeAxisLabel(scene, zUp) {
 
+	if (zUp == true) {
+		var first = "Y";
+		var second = "Z";
+		var third = "X";
+	} else {
+		var first = "X";
+		var second = "Y";
+		var third = "Z";
+	}
 	// add 3D text
 	var materialFront = new THREE.MeshBasicMaterial({
 		color : 0xff0000
@@ -8,9 +17,9 @@ function makeAxisLabel(scene) {
 		color : 0x000088
 	});
 	var materialArray = [ materialFront, materialSide ];
-	var textGeom = new THREE.TextGeometry("X", {
-		size : gridSize/10,
-		height : gridSize/100,
+	var textGeom = new THREE.TextGeometry(first, {
+		size : gridSize / 10,
+		height : gridSize / 100,
 		curveSegments : 3,
 		font : "helvetiker",
 		weight : "bold",
@@ -30,12 +39,11 @@ function makeAxisLabel(scene) {
 	textGeom.computeBoundingBox();
 	var textWidth = textGeom.boundingBox.max.x - textGeom.boundingBox.min.x;
 
-	//textMesh.position.set(-0.5 * textWidth, 50, 100);
+	// textMesh.position.set(-0.5 * textWidth, 50, 100);
 	textMesh.position.set(gridSize, 0, 0);
 	textMesh.rotation.x = -Math.PI / 4;
 	scene.add(textMesh);
 
-	
 	// add 3D text
 	var materialFront = new THREE.MeshBasicMaterial({
 		color : 0xff0000
@@ -44,7 +52,7 @@ function makeAxisLabel(scene) {
 		color : 0x000088
 	});
 	var materialArray = [ materialFront, materialSide ];
-	var textGeom = new THREE.TextGeometry("Y", {
+	var textGeom = new THREE.TextGeometry(second, {
 		size : gridSize / 10,
 		height : gridSize / 100,
 		curveSegments : 3,
@@ -72,7 +80,7 @@ function makeAxisLabel(scene) {
 	scene.add(textMesh);
 
 	// Z
-	var textGeom = new THREE.TextGeometry("Z", {
+	var textGeom = new THREE.TextGeometry(third, {
 		size : gridSize / 10,
 		height : gridSize / 100,
 		curveSegments : 3,
@@ -98,5 +106,4 @@ function makeAxisLabel(scene) {
 	textMesh.position.set(0, 0, gridSize);
 	textMesh.rotation.z = -Math.PI / 4;
 	scene.add(textMesh);
-
 }
